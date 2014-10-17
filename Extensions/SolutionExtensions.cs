72f805fdb9808d6577720e49fdf71b73fbfc93a7
Josh Varty <joshvarty@gmail.com>
2014-10-16 21:57:17 -0400

    Added extension method for Solution.GetName()
    We'll use this when analyzing the folder structure. TBH, I'm surprised
    we can't find the name of the solution via Roslyn.

--- Extensions/SolutionExtensions.cs
+++ Extensions/SolutionExtensions.cs
@@ -2,11 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using System.IO;
 
 namespace SourceBrowser.Generator.Extensions
 {
     public static class SolutionExtensions
     {
+        /// <summary>
+        /// Returns the name of the solution without the ".sln" suffix.
+        /// </summary>
+        public static string GetName(this Solution solution)
+        {
+            const string SOLUTION_SUFFIX = ".sln";
+            var fileName = Path.GetFileName(solution.FilePath);
+
+            if (fileName.EndsWith(SOLUTION_SUFFIX))
+                fileName = fileName.Remove(fileName.IndexOf(SOLUTION_SUFFIX), SOLUTION_SUFFIX.Length); 
+
+            return fileName;
+        }
     }
 }
